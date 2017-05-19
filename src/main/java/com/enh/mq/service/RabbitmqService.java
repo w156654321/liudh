1ef0145123454d41cab28ed4f74c088b2b0539d0
w156654321 <183958539@qq.com>
2017-05-19 14:24:49 +0800

    多队列

--- src/main/java/com/enh/mq/service/RabbitmqService.java
+++ src/main/java/com/enh/mq/service/RabbitmqService.java
@@ -12,8 +12,10 @@ package com.enh.mq.service;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 
+import java.util.Date;
+
 public class RabbitmqService implements MessageListener {
     public void onMessage(Message message) {
-        System.out.println("消息消费者client消费： = " + message.toString());
+        System.out.println("消息消费者client消费： = " + message.toString()+"时间:"+new Date());
     }
 }
